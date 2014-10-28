817a4b0166d95bed990a1420c0e7921fc178756e
Sebastin Kolman <saby83@gmail.com>
2014-10-28 01:05:39 -0400

    passwords for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
   before_save { self.email = email.downcase }
   validates(:name, presence: true)
   validates(:email, presence: true)
+  validates :password, length: { minimum: 6 }
 end
